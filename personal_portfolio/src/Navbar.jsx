6efc70f39ad8bbe6a3ac4dd6d604741498d03d63
Muthukumar V <www.goe2004@gmail.com>
2025-07-17 23:26:00 +0530

    changes

--- personal_portfolio/src/Navbar.jsx
+++ personal_portfolio/src/Navbar.jsx
@@ -1,4 +1,19 @@
+import { useState } from "react";
+
 function Navbar() {
+  const [options, setOptions] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  
+  function choose(e) {
+    e.preventDefault();
+    setIsOpen(!isOpen);
+    if (!isOpen) {
+      setOptions(["Option 1", "Option 2", "Option 3"]);
+    } else {
+      setOptions([]);
+    }
+  }
+
   return (
     <div>
       Navbar
@@ -8,7 +23,12 @@ function Navbar() {
             <a href="/">Home</a>
           </li>
           <li>
-            <a href="/about">About</a>
+            <a onClick={choose} href="/about">About</a>
+            {options.map((option, index) => (
+              <div key={index} className="option-item">
+                {option}
+              </div>
+            ))}
           </li>
         </ul>
       </nav>
