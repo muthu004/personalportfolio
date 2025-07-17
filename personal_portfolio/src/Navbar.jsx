3ed45f534168466a2871491dd22906e5a021d356
Muthukumar V <www.goe2004@gmail.com>
2025-07-17 22:03:40 +0530

    first commit

---
+++ personal_portfolio/src/Navbar.jsx
@@ -0,0 +1,19 @@
+function Navbar() {
+  return (
+    <div>
+      Navbar
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
